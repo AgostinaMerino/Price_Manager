73c1499ff36d6f5e9d7b5cf90f04fc5b08bafdac
AgostinaMerino <agosm.am@gmail.com>
2026-05-18 13:37:15 +0000

    Dia 5 - Configuracion final del sistema

--- src/price_manager/main.py
+++ src/price_manager/main.py
@@ -1,4 +1,5 @@
-
+from price_manager.migrations.migrations import migrar_datos
+from price_manager.models.models import crear_tablas
 from price_manager.preload_data.preload_data import precargar_datos
 from price_manager.ui.console import main as ejecutar_menu
 
@@ -6,14 +7,23 @@ from price_manager.ui.console import main as ejecutar_menu
 desactivar_git_push = False
 
 
-def main(import_default_data: bool = False) -> None:
-  """Ejecuta el sistema Price Manager.
+def inicializar_base_datos() -> None:
+  """Inicializa tablas y datos base."""
 
-  Args:
-    import_default_data (bool): indica si deben precargarse los CSV.
-  """
+  crear_tablas()
+  precargar_datos()
+
+  migrar_datos(
+    "/content/price_manager/src/price_manager/migrations/csv",
+    "/content/price_manager/src/price_manager/migrations/sql",
+  )
+
+
+def main(import_default_data: bool = False) -> None:
+  """Ejecuta el sistema."""
+
   if import_default_data:
-    precargar_datos()
+    inicializar_base_datos()
 
   ejecutar_menu()
 
